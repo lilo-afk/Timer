c9b95de343337b5f4772e2cef109b948fb5c5077
Murilo <80017589+lilo-afk@users.noreply.github.com>
2023-01-29 15:35:05 -0300

    added timer functionality

--- scripts.js
+++ scripts.js
@@ -1,31 +1,91 @@
-const startButton = document.querySelector('[data-start]')
-const pauseButton = document.querySelector('[data-pause]')
-const stopButton = document.querySelector('[data-stop]')
-const timeElement = document.querySelector('[data-time]')
+const startButton = document.querySelector("[data-start]");
+const pauseButton = document.querySelector("[data-pause]");
+const stopButton = document.querySelector("[data-stop]");
+const timeElement = document.querySelector("[data-time]");
 
 let seconds = 0,
     minutes = 0,
     hours = 0;
     let interval;
 
-const renderTimer = (seconds, minutes, hours) => {
-    timeElement.innerHTML = hours +;
+const renderTime = (seconds, minutes, hours) => {
+
+    const hoursValue = hours < 10 ? "0" + hours : hours;
+    const minutesValue = minutes < 10 ? "0" + minutes : minutes;
+    const secondsValue = seconds < 10 ? "0" + seconds : seconds;
+
+    timeElement.innerHTML = hoursValue + ":" + minutesValue + ":" + secondsValue; 
+
 };
 
-const startTime = () => {
+const startTime = (startValue) => {
+    startButton.setAttribute("disabled", "true");
+    pauseButton.removeAttribute("disabled");
+    stopButton.removeAttribute("disabled");
 
-    interval = setInterval(() => {
-        seconds++;
-        while(seconds === 60){
-            seconds = 0;
-            minutes++;
-        }
 
-        while( minutes === 60) {
-            minutes = 0;
-            hours++;
-        } 
-    }, 1000);
-} 
+    if (startValue === "start" || startValue === "restart"){
+        (seconds = seconds != 0 ? -1 : 0), (minutes = 0), (hours = 0);
+        interval = setInterval(() => {
+            seconds++;
+            while(seconds === 60){
+                seconds = 0;
+                minutes++;
+            }
+            while( minutes === 60) {
+                minutes = 0;
+                hours++;
+            }
+        renderTime(seconds, minutes, hours);
+        }, 1000);
+    }else if(startValue === "continue"){
+        interval = setInterval(() => {
+            seconds++;
+            while(seconds === 60){
+                seconds = 0;
+                minutes++;
+            }
+    
+            while( minutes === 60) {
+                minutes = 0;
+                hours++;
+            }
+        renderTime(seconds, minutes, hours);
+        }, 1000);
+    }
+};   
 
-startTime ();
+const pauseTime = () => {
+    startButton.removeAttribute("disabled");
+    startButton.setAttribute("data-start","continue");
+    pauseButton.setAttribute("disabled", "true");
+    stopButton.setAttribute("disabled", "true");
+    startButton.innerHTML = "Retomar";
+    
+    clearInterval(interval);
+};
+
+const stopTime = () => {
+
+    startButton.removeAttribute("disabled");
+    startButton.setAttribute("data-start", "restart");
+    pauseButton.setAttribute("disabled", "true");
+    startButton.innerHTML = ("Reiniciar");
+
+    clearInterval(interval);
+}
+
+startButton.onclick = () => {    
+    const startValue = startButton.getAttribute("data-start");
+    startTime(startValue);
+};
+
+pauseButton.onclick = () => {
+
+    pauseTime();
+};
+
+stopButton.onclick = () => {
+
+    stopTime();
+};
